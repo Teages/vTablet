44b735d66f233fe2eaf361cd48b6b81a161df9da
Teages <Teages@teages.xyz>
2023-03-14 15:52:39 +0800

    hide command window

--- server/tools.go
+++ server/tools.go
@@ -4,6 +4,7 @@ import (
 	"os"
 	"os/exec"
 	"strings"
+	"syscall"
 
 	"github.com/Teages/vTablet/internal/console"
 )
@@ -17,6 +18,7 @@ func cmd(path string, args ...string) {
 	exePath := strings.TrimSpace(path)
 	arg := `/c ` + exePath + " " + strings.Join(args, " ")
 	e := exec.Command("cmd", arg)
+	e.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
 	console.Log("Running: %s %s", exePath, arg)
 	e.Stdout = os.Stdout
 	e.Stderr = os.Stderr
